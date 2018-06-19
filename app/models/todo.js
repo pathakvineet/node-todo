bb82362f1d355c893ba8a6c8279c2d5e44412d41
Vineet <pathakvineet3@gmail.com>
2018-06-19 12:26:28 +0530

    Updated for the map

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,3 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Todo', {
-    text: {
-        type: String,
-        default: ''
-    }
-});
+module.exports = mongoose.model('', {}, 'allStates');
